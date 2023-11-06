847c4ef6b4b5bfe759cb8c7971a745b4e66cf7e7
Zaim hasan <zaimhasanchy@gmail.com>
2023-11-06 21:15:41 +0600

    first commit

---
+++ src/test/java/com/selenium/BrowserLaunchTest.java
@@ -0,0 +1,15 @@
+package com.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserLaunchTest {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://google.com");
+        driver.quit();
+    }
+}
